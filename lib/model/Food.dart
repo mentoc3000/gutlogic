9d574ed878c684d472c23deb17bbb54b050a98d2
JP Sheehan <jp.sheehan2@gmail.com>
2019-02-07 18:29:15 -0500

    Add food class

---
+++ lib/model/Food.dart
@@ -0,0 +1,16 @@
+class Food {
+  String name;
+  List<String> irritants;
+
+  Food({
+    this.name,
+    this.irritants
+  });
+
+  factory Food.fromJson(Map<String, dynamic> json) {
+    return new Food(
+      name: json['name'],
+      irritants: json['irritants']
+    );
+  }
+}
