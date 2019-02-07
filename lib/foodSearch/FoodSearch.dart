bcab05488a320fbee12af9009fe2258e6a80f13e
JP Sheehan <jp.sheehan2@gmail.com>
2019-02-07 18:25:06 -0500

    Make FoodSearch stateful

--- lib/foodSearch/FoodSearch.dart
+++ lib/foodSearch/FoodSearch.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:gi_bliss/helpers/PlaceholderWidget.dart';
 
-class FoodSearch extends StatelessWidget {
+class FoodSearch extends StatefulWidget {
+  @override
+  _FoodSearchState createState() {
+    return _FoodSearchState();
+  }
+}
+
+class _FoodSearchState extends State<FoodSearch> {
+
   @override
   Widget build(BuildContext context) {
-    return PlaceholderWidget(Colors.grey);
+    return PlaceholderWidget(Colors.indigo);
   }
 }
