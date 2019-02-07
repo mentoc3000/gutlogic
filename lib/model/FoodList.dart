74ad6a9a44795ad7f6fde5fb0259d2d5047bbfed
JP Sheehan <jp.sheehan2@gmail.com>
2019-02-07 18:32:36 -0500

    Add FoodList class

---
+++ lib/model/FoodList.dart
@@ -0,0 +1,17 @@
+import 'Food.dart';
+
+class FoodList {
+  List<Food> foods = new List();
+
+  FoodList({
+    this.foods
+  });
+
+  factory FoodList.fromJson(List<dynamic> parsedJson) {
+    List<Food> foods = new List<Food>();
+
+    foods = parsedJson.map((i) => Food.fromJson(i)).toList();
+
+    return new FoodList(foods: foods);
+  }
+}
