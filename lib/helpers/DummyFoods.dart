9d4d117702dddfb931193d4477327621529f2309
JP Sheehan <jp.sheehan2@gmail.com>
2019-02-08 17:44:14 -0500

    Replace irritant with string

--- lib/helpers/DummyFoods.dart
+++ lib/helpers/DummyFoods.dart
@@ -2,9 +2,18 @@ import 'package:gi_bliss/model/Food.dart';
 import 'package:gi_bliss/model/FoodList.dart';
 
 class DummyFoods {
-  static final _artichoke = Food(name: 'Artichoke', irritants: ['Fructans', 'Fructose']);
-  static final _creamCheese = Food(name: 'Cream Cheese', irritants: ['Lactose']);
-  static final _egg = Food(name: 'Egg', irritants: []);
+  static final _artichoke = Food(
+    name: 'Artichoke', 
+    irritants: ['Fructans', 'Fructose']
+  );
+  static final _creamCheese = Food(
+    name: 'Cream Cheese', 
+    irritants: ['Lactose']
+  );
+  static final _egg = Food(
+    name: 'Egg', 
+    irritants: []
+  );
 
   static final _foods = [_artichoke, _creamCheese, _egg];
 
