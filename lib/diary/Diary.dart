44de09e72a4d462c185d7835832fac5c1f54a633
JP Sheehan <jp.sheehan2@gmail.com>
2019-02-09 11:44:23 -0500

    Add dummy diary entries

--- lib/diary/Diary.dart
+++ lib/diary/Diary.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar/flutter_calendar.dart';
+import 'package:gi_bliss/model/DiaryEntry.dart';
+import 'package:gi_bliss/model/Food.dart';
 
 class Diary extends StatefulWidget {
   @override
@@ -8,6 +10,34 @@ class Diary extends StatefulWidget {
 
 class DiaryState extends State<Diary> {
 
+  List<FoodAndDrinkEntry> foodAndDrinkEntries = [
+    FoodAndDrinkEntry(
+      dateTime: DateTime(2019, 2, 7, 8, 14), 
+      food: Food(name: "Bread", irritants: ['Fructans']),
+      quantity: "1 slice"
+    ),
+    FoodAndDrinkEntry(
+      dateTime: DateTime(2019, 2, 7, 8, 14), 
+      food: Food(name: "Egg", irritants: []),
+      quantity: "1 each"
+    )
+  ];
+
+  List<BowelMovementEntry> bowelMovementEntries = [
+    BowelMovementEntry(
+      dateTime: DateTime(2019, 2, 7, 11, 31),
+      consistency: 4,
+      volume: 2
+    )
+  ];
+
+  List<MedicineEntry> medicineEntries = [
+    MedicineEntry(
+      dateTime: DateTime(2019, 2, 7, 12, 0),
+      medicine: "Pro-8",
+      dose: "1 pill"
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
