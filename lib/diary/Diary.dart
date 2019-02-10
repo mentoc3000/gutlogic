044aaa4a9697e27d1d37d6b03bc36c09d236fccf
JP Sheehan <jp.sheehan2@gmail.com>
2019-02-09 20:37:21 -0500

    Add basic entry header

--- lib/diary/Diary.dart
+++ lib/diary/Diary.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_calendar/flutter_calendar.dart';
 import 'package:gi_bliss/model/DiaryEntry.dart';
 import 'package:gi_bliss/model/Food.dart';
+import 'package:gi_bliss/helpers/Dummy.dart';
 
 class Diary extends StatefulWidget {
   @override
@@ -9,35 +10,6 @@ class Diary extends StatefulWidget {
 }
 
 class DiaryState extends State<Diary> {
-
-  List<FoodAndDrinkEntry> foodAndDrinkEntries = [
-    FoodAndDrinkEntry(
-      dateTime: DateTime(2019, 2, 7, 8, 14), 
-      food: Food(name: "Bread", irritants: ['Fructans']),
-      quantity: "1 slice"
-    ),
-    FoodAndDrinkEntry(
-      dateTime: DateTime(2019, 2, 7, 8, 14), 
-      food: Food(name: "Egg", irritants: []),
-      quantity: "1 each"
-    )
-  ];
-
-  List<BowelMovementEntry> bowelMovementEntries = [
-    BowelMovementEntry(
-      dateTime: DateTime(2019, 2, 7, 11, 31),
-      consistency: 4,
-      volume: 2
-    )
-  ];
-
-  List<MedicineEntry> medicineEntries = [
-    MedicineEntry(
-      dateTime: DateTime(2019, 2, 7, 12, 0),
-      medicine: "Pro-8",
-      dose: "1 pill"
-    )
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -51,9 +23,45 @@ class DiaryState extends State<Diary> {
         ),
       ),
       body: ListView(
-        children: <Widget>[],
+        children: <Widget>[
+          EntryHeader(title: "Breakfast", color: Colors.green,)
+        ],
       ),
     );
   }
 }
 
+class EntryHeader extends StatelessWidget {
+  final Color color;
+  final String title;
+
+  EntryHeader({this.title, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 100.0,
+            // margin: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(3.0),
+            decoration: new BoxDecoration(
+              border: new Border.all(color: Colors.black)
+            ),
+            child: new Text("9:35 am"),
+          ),
+          Container(
+            // margin: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(3.0),
+            decoration: new BoxDecoration(
+              border: new Border.all(color: Colors.black),
+              color: color
+            ),
+            child: new Text(title),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/helpers/Dummy.dart
+++ lib/helpers/Dummy.dart
@@ -1,5 +1,6 @@
 import 'package:gi_bliss/model/Food.dart';
 import 'package:gi_bliss/model/FoodList.dart';
+import 'package:gi_bliss/model/DiaryEntry.dart';
 
 class Dummy {
   static final _artichoke = Food(
@@ -18,4 +19,35 @@ class Dummy {
   static final _foods = [_artichoke, _creamCheese, _egg];
 
   static final foodList = FoodList(foods: _foods);
+
+
+
+  List<FoodAndDrinkEntry> foodAndDrinkEntries = [
+    FoodAndDrinkEntry(
+      dateTime: DateTime(2019, 2, 7, 8, 14), 
+      food: Food(name: "Bread", irritants: ['Fructans']),
+      quantity: "1 slice"
+    ),
+    FoodAndDrinkEntry(
+      dateTime: DateTime(2019, 2, 7, 8, 14), 
+      food: Food(name: "Egg", irritants: []),
+      quantity: "1 each"
+    )
+  ];
+
+  List<BowelMovementEntry> bowelMovementEntries = [
+    BowelMovementEntry(
+      dateTime: DateTime(2019, 2, 7, 11, 31),
+      consistency: 4,
+      volume: 2
+    )
+  ];
+
+  List<MedicineEntry> medicineEntries = [
+    MedicineEntry(
+      dateTime: DateTime(2019, 2, 7, 12, 0),
+      medicine: "Pro-8",
+      dose: "1 pill"
+    )
+  ];
 }
