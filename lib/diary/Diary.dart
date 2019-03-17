043c4d2f465e4d4b49d3b4cfd48adcdbc046ce07
JP Sheehan <jp.sheehan2@gmail.com>
2019-03-17 17:50:11 -0400

    Refactor EntryHeader

--- lib/diary/Diary.dart
+++ lib/diary/Diary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gi_bliss/genericViews/itemTileLarge.dart';
+import 'package:gi_bliss/genericViews/ItemTileLarge.dart';
+import 'EntryHeader.dart';
 import 'package:flutter_calendar/flutter_calendar.dart';
 import 'package:gi_bliss/model/DiaryEntry.dart';
 import 'package:gi_bliss/model/Food.dart';
@@ -34,39 +35,3 @@ class DiaryState extends State<Diary> {
     );
   }
 }
-
-class EntryHeader extends StatelessWidget {
-  final Color color;
-  final String title;
-
-  EntryHeader({this.title, this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: 80.0,
-            margin: const EdgeInsets.fromLTRB(15.0, 15.0, 0.0, 15.0),
-            padding: const EdgeInsets.all(3.0),
-            decoration: new BoxDecoration(
-              border: new Border.all(color: Colors.black)
-            ),
-            child: new Text("9:35 am"),
-          ),
-          Expanded(child: Container(
-            margin: const EdgeInsets.fromLTRB(0.0, 15.0, 15.0, 15.0),
-            padding: const EdgeInsets.all(3.0),
-            decoration: new BoxDecoration(
-              border: new Border.all(color: Colors.black),
-              color: color
-            ),
-            child: new Text(title),
-          )),
-        ],
-      ),
-    );
-  }
-}
-
