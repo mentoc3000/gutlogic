9e61848b9dc5a52eb8460a8ec4b370bcd76ebae2
JP Sheehan <jp.sheehan2@gmail.com>
2019-03-19 20:45:15 -0400

    Add diary tiles

---
+++ lib/diary/diary_tiles.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:gut_ai/model/diary_entry.dart';
+import 'package:gut_ai/generic_widgets/item_tile.dart';
+
+class FoodAndDrinkTileLarge extends ItemTileLarge {
+  final FoodAndDrinkEntry entry;
+
+  FoodAndDrinkTileLarge({this.entry}):
+    super(
+      heading: entry.food.name, 
+      subheading: entry.food.irritants.join(', ')
+    );
+
+}
+
+class BowelMovementTile extends ItemTileLarge {
+  final BowelMovementEntry entry;
+
+  BowelMovementTile({this.entry}):
+    super(
+      heading: 'Type ' + entry.bowelMovement.type.toString(), 
+      subheading: 'Volume: ' + entry.bowelMovement.volume.toString()
+    );
+
+}
+
+class MedicineTile extends ItemTileLarge {
+  final MedicineEntry entry;
+
+  MedicineTile({this.entry}):
+    super(
+      heading: entry.medicine.name, 
+      subheading: entry.dose
+    );
+
+}
