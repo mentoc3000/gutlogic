acb18f11d0088e495c81445bb64bfe503509565a
JP Sheehan <jp.sheehan2@gmail.com>
2019-03-20 21:04:31 -0400

    Compose diary entry tiles

--- lib/diary/diary_tiles.dart
+++ lib/diary/diary_tiles.dart
@@ -2,35 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:gut_ai/model/diary_entry.dart';
 import 'package:gut_ai/generic_widgets/item_tile.dart';
 
-class FoodAndDrinkTileLarge extends ItemTileLarge {
+class FoodAndDrinkTileLarge extends StatelessWidget {
   final MealEntry entry;
 
-  FoodAndDrinkTileLarge({this.entry}):
-    super(
-      heading: entry.meal.name, 
-      subheading: entry.meal.ingredients.fold('', (value, element) => value + ', ' + element.food.name)
+  FoodAndDrinkTileLarge({this.entry});
+  
+  @override
+  Widget build(BuildContext context) {
+    return ItemTileLarge (
+      heading: entry.meal.name,
+      subheading: entry.meal.ingredients.fold('', (value, element) => value + ', ' + element.food.name),
     );
+  }
 
 }
 
-class BowelMovementTile extends ItemTileLarge {
+class BowelMovementTile extends StatelessWidget {
   final BowelMovementEntry entry;
 
-  BowelMovementTile({this.entry}):
-    super(
+  BowelMovementTile({this.entry});
+
+  @override
+  Widget build(BuildContext context) {
+    return ItemTileLarge (
       heading: 'Type ' + entry.bowelMovement.type.toString(), 
       subheading: 'Volume: ' + entry.bowelMovement.volume.toString()
     );
+  }
 
 }
 
-class MedicineTile extends ItemTileLarge {
+class MedicineTile extends StatelessWidget {
   final MedicineEntry entry;
 
-  MedicineTile({this.entry}):
-    super(
+  MedicineTile({this.entry});
+
+  @override
+  Widget build(BuildContext context) {
+    return ItemTileLarge (
       heading: entry.medicine.name, 
       subheading: entry.dose
     );
+  }
 
 }
