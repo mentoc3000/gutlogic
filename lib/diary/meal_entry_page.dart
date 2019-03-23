cc85020825a195b00c6b16cebbde6229923b1964
JP Sheehan <jp.sheehan2@gmail.com>
2019-03-23 17:31:32 -0400

    Add listview to meal entry page

--- lib/diary/meal_entry_page.dart
+++ lib/diary/meal_entry_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gut_ai/model/diary_entry.dart';
 import 'package:gut_ai/helpers/placeholder_widget.dart';
+import 'package:gut_ai/generic_widgets/item_tile.dart';
 
 class MealEntryPage extends StatefulWidget {
   static String tag = 'meal-entry-page';
@@ -22,7 +23,15 @@ class MealEntryPageState extends State<MealEntryPage> {
         centerTitle: true,
         title: Text(widget.entry.meal.name),
       ),
-      body: PlaceholderWidget(Colors.cyanAccent)
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Divider(),
+        itemCount: 1,
+        itemBuilder: (context, index) => Padding(
+          padding: EdgeInsets.all(1.0),
+          child: GutAIListTile(heading: 'Placeholder',),
+        ),
+        padding: EdgeInsets.all(0.0),
+      ),
     );
   }
 }
