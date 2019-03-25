50b10ae4ea29c7083c6c91db8fdf3512a2bbfc00
JP Sheehan <jp.sheehan2@gmail.com>
2019-03-24 20:24:54 -0400

    Show type label

--- lib/diary/bm_type_slider.dart
+++ lib/diary/bm_type_slider.dart
@@ -4,9 +4,19 @@ import 'package:flutter/material.dart';
 
 class BMTypeSliderTile extends StatefulWidget {
 
-  final int minimum = 0;
+  final int minimum = 1;
   final int maximum = 8;
   final int type;
+  final List<String> descriptions = [
+    'Type 1',
+    'Type 2',
+    'Type 3',
+    'Type 4',
+    'Type 5',
+    'Type 6',
+    'Type 7',
+    'Type 8',
+  ];
 
   BMTypeSliderTile({this.type: 5});
 
@@ -17,11 +27,13 @@ class BMTypeSliderTile extends StatefulWidget {
 class _BMTypeSliderTileState extends State<BMTypeSliderTile> {
 
   int value;
+  String description;
 
   @override
   void initState() {
     super.initState();
     this.value = widget.type;
+    this.description = widget.descriptions[widget.type - 1];
   }
 
   @override
@@ -35,8 +47,12 @@ class _BMTypeSliderTileState extends State<BMTypeSliderTile> {
             max: widget.maximum.toDouble(),
             value: value.toDouble(),
             divisions: widget.maximum - widget.minimum,
-            onChanged: (newValue) => setState(() => value=newValue.toInt()),
-          )
+            onChanged: (newValue) => setState(() {
+              value=newValue.toInt();
+              description=widget.descriptions[value - 1];
+            }),
+          ),
+          Text(description)
         ],
       ),
     );
