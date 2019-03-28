bde945d2a108cee746bc825df0c3071b7b9be234
JP Sheehan <jp.sheehan2@gmail.com>
2019-03-28 17:07:23 -0400

    Use aws cognito example pages

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:gut_ai/main_tabs.dart';
 import 'package:gut_ai/account/account_page.dart';
 import 'package:gut_ai/diary/diary_page.dart';
 import 'package:gut_ai/food_search/food_search_page.dart';
-import 'package:gut_ai/login/login_page.dart';
+// import 'package:gut_ai/login/login_page.dart';
 import 'package:gut_ai/diary/meal_entry_page.dart';
+import 'package:gut_ai/login/aws_login_example.dart';
 
 void main() => runApp(GiBlissApp());
 
@@ -22,7 +23,7 @@ class GiBlissApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: LoginPage(),
+        body: LoginScreen(),
       ),
       routes: routes,
     );
