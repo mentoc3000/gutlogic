8664ef57e8204c665e81bbc4349bbd8c40909629
JP Sheehan <jp.sheehan2@gmail.com>
2019-04-09 20:46:51 -0400

    Add logout button

--- lib/account/account_page.dart
+++ lib/account/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gut_ai/login/aws_login_example.dart';
 
 class AccountPage extends StatefulWidget {
   static String tag = 'account-page';
@@ -17,7 +18,24 @@ class AccountPageState extends State<AccountPage> {
       ),
       body: ListView(
         children: <Widget>[
-          ListTile(title: Text("Placeholder"),)
+          ListTile(title: Text("Placeholder"),),
+          new Divider(),
+          new Center(
+            child: new InkWell(
+              child: new Text(
+                'Logout',
+                style: new TextStyle(color: Colors.blueAccent),
+              ),
+              onTap: () async {
+                final _userService = new UserService(userPool);
+                await _userService.signOut();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen())
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
