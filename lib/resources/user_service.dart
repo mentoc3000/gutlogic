b504e5ace4bd425d50c5b59a17fa57dc7935d1a9
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-11 08:13:25 -0400

    Test lambda endpoint

--- lib/resources/user_service.dart
+++ lib/resources/user_service.dart
@@ -82,29 +82,6 @@ class User {
   }
 }
 
-class CounterService {
-  AwsSigV4Client awsSigV4Client;
-  CounterService(this.awsSigV4Client);
-
-  /// Retrieve user's previous count from Lambda + DynamoDB
-  Future<Counter> getCounter() async {
-    final signedRequest =
-        new SigV4Request(awsSigV4Client, method: 'GET', path: '/counter');
-    final response =
-        await http.get(signedRequest.url, headers: signedRequest.headers);
-    return new Counter.fromJson(json.decode(response.body));
-  }
-
-  /// Increment user's count in DynamoDB
-  Future<Counter> incrementCounter() async {
-    final signedRequest =
-        new SigV4Request(awsSigV4Client, method: 'PUT', path: '/counter');
-    final response =
-        await http.put(signedRequest.url, headers: signedRequest.headers);
-    return new Counter.fromJson(json.decode(response.body));
-  }
-}
-
 class UserService {
   static final UserService _singleton = new UserService._internal();
 
