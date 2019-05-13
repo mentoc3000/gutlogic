e1371a2e02182a760429a103e3f7a39cbf7f0bac
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-13 07:49:02 -0400

    Add symptomType Bloc

---
+++ lib/blocs/symptom_type_bloc.dart
@@ -0,0 +1,28 @@
+import '../resources/symptom_type_repository.dart';
+// import 'package:rxdart/rxdart.dart';
+import 'dart:async';
+import '../models/symptom.dart';
+
+class SymptomTypeBloc {
+  // final AppSyncService appSyncService;
+  final SymptomTypeRepository symptomTypeRepository = SymptomTypeRepository();
+
+  // Use broadcast stream because stream in search gets subscribed to multiple times
+  final _symptomTypeController = StreamController<List<SymptomType>>.broadcast();
+
+  Stream<List<SymptomType>> get allSymptomTypes => _symptomTypeController.stream;
+
+  fetchAllSymptomTypes() async {
+    List<SymptomType> foods = await symptomTypeRepository.fetchAllSymptomTypes();
+    _symptomTypeController.sink.add(foods);
+  }
+
+  fetchQuery(String query) async {
+    List<SymptomType> foods = await symptomTypeRepository.fetchQuery(query);
+    _symptomTypeController.sink.add(foods);
+  }
+
+  dispose() {
+    _symptomTypeController.close();
+  }
+}
