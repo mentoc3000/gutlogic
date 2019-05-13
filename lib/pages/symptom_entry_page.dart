f2c38be2a17efd943c3eb948ccf6e9e3c3987cfd
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-12 21:24:57 -0400

    Add datetime to symptom

--- lib/pages/symptom_entry_page.dart
+++ lib/pages/symptom_entry_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/diary_entry.dart';
 import '../widgets/placeholder_widget.dart';
+import '../widgets/datetime_view.dart';
 
 class SymptomEntryPage extends StatefulWidget {
   static String tag = 'symptom-entry-page';
@@ -14,15 +15,29 @@ class SymptomEntryPage extends StatefulWidget {
 }
 
 class SymptomEntryPageState extends State<SymptomEntryPage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Symptom'),
+    List<Widget> items = [
+      DatetimeView(
+        date: widget.entry.dateTime,
       ),
-      body: PlaceholderWidget(Colors.yellowAccent)
+    ];
+
+    return Theme(
+      data: Theme.of(context).copyWith(primaryColor: Colors.red),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Symptom'),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) =>
+              Padding(padding: EdgeInsets.all(1.0), child: items[index]),
+          padding: EdgeInsets.all(0.0),
+        ),
+      ),
     );
   }
 }
+
