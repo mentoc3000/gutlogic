f27cd67f0f1d5a38033298326c2b2ec2c627c8d5
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-13 07:45:14 -0400

    Add symptom repository

---
+++ lib/resources/symptom_type_repository.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import '../models/symptom.dart';
+
+class SymptomTypeRepository {
+
+  SymptomTypeRepository();
+
+  Future<List<SymptomType>> fetchAllSymptomTypes() async {
+    await Future.delayed(Duration(seconds: 1));
+    return SymptomType.values;
+  }
+
+  Future<List<SymptomType>> fetchQuery(String query) async {
+    if (query == '') {
+      return fetchAllSymptomTypes();
+    }
+    return SymptomType.values
+        .where((f) => f.toString().contains(query))
+        .toList();
+  }
+}
---
+++ test/resources/symptom_type_repository_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gut_ai/resources/symptom_type_repository.dart';
+import 'package:gut_ai/models/symptom.dart';
+
+void main() {
+  group('Symptom Type Repository', () {
+    test('fetches all symptom types', () async {
+      SymptomTypeRepository symptomTypeRepository = SymptomTypeRepository();
+      List<SymptomType> symptomTypes = await symptomTypeRepository.fetchAllSymptomTypes();
+      expect(symptomTypes.length, 3);
+      expect(symptomTypes.contains(SymptomType.gas), true);
+      expect(symptomTypes.contains(SymptomType.constipation), true);
+      expect(symptomTypes.contains(SymptomType.bloating), true);
+    });
+  });
+}
+
