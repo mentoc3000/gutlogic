38f0e1a88ad2d9084dc886b5de0b648584100ac2
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-18 18:16:18 -0400

    Make SymptomTypeBloc a SearchableBloc

--- lib/blocs/bloc_interfaces.dart
+++ lib/blocs/bloc_interfaces.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import '../models/model_interfaces.dart';
+import '../resources/app_sync_service.dart';
 
 abstract class GutAiBloc {
 
@@ -7,6 +8,7 @@ abstract class GutAiBloc {
 
 abstract class SearchableBloc extends GutAiBloc {
   Stream<List<Searchable>> get all;
+  SearchableBloc(AppSyncService appSyncService);
   void fetchAll();
   void fetchQuery(String query);
 }
--- lib/blocs/symptom_type_bloc.dart
+++ lib/blocs/symptom_type_bloc.dart
@@ -2,13 +2,17 @@ import '../resources/symptom_type_repository.dart';
 // import 'package:rxdart/rxdart.dart';
 import 'dart:async';
 import '../models/symptom.dart';
+import 'bloc_interfaces.dart';
+import '../resources/app_sync_service.dart';
 
-class SymptomTypeBloc {
+class SymptomTypeBloc implements SearchableBloc{
   // final AppSyncService appSyncService;
   final SymptomTypeRepository symptomTypeRepository = SymptomTypeRepository();
 
   // Use broadcast stream because stream in search gets subscribed to multiple times
   final _symptomTypeController = StreamController<List<SymptomType>>.broadcast();
+
+  SymptomTypeBloc(AppSyncService appSyncService);
 
   Stream<List<SymptomType>> get all => _symptomTypeController.stream;
 
