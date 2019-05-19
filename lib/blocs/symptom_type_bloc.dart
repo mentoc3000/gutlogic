a400e33d7e3033c691609bb4adfaeb55f523fee8
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-19 13:31:52 -0400

    Use GutAiSearchDelegate in symptom type search

--- lib/blocs/symptom_type_bloc.dart
+++ lib/blocs/symptom_type_bloc.dart
@@ -3,11 +3,10 @@ import '../resources/symptom_type_repository.dart';
 import 'dart:async';
 import '../models/symptom.dart';
 import 'bloc_interfaces.dart';
-import '../resources/app_sync_service.dart';
 
 class SymptomTypeBloc extends SearchableBloc{
 
-  SymptomTypeBloc(AppSyncService appSyncService)
+  SymptomTypeBloc()
       : super(repository: SymptomTypeRepository());
 
   Stream<List<SymptomType>> get all => controller.stream.cast();
--- lib/pages/symptom_entry_page.dart
+++ lib/pages/symptom_entry_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gut_ai/blocs/symptom_type_bloc.dart';
 import '../models/diary_entry.dart';
-import '../models/symptom.dart';
-import '../widgets/placeholder_widget.dart';
 import '../widgets/datetime_view.dart';
+import 'gutai_search_delegate.dart';
 
 class SymptomEntryPage extends StatefulWidget {
   static String tag = 'symptom-entry-page';
@@ -98,11 +97,17 @@ class SymptomEntryPageState extends State<SymptomEntryPage> {
   }
 
   void showFoodSearch(BuildContext context) {
+
+    final symptomTypeBloc = SymptomTypeBloc();
+
+    final onSelect = (symptomType) => this._symptomEntry.symptom.symptomType = symptomType;
+
     showSearch(
       context: context,
-      delegate: SymptomSearchDelegate(onSelect: (symptomType) {
-        this._symptomEntry.symptom.symptomType = symptomType;
-      }),
+      delegate: GutAiSearchDelegate(
+        searchableBloc: symptomTypeBloc,
+        onSelect: onSelect,
+      ),
     );
   }
 
@@ -153,122 +158,5 @@ class SeverityIndicator extends StatelessWidget {
     return Column(
       children: <Widget>[image, Text(label)],
     );
-  }
-}
-
-class SymptomSearchDelegate extends SearchDelegate {
-  SymptomTypeBloc _symptomTypeBloc;
-  final void Function(SymptomType) onSelect;
-
-  SymptomSearchDelegate({this.onSelect}) {
-    _symptomTypeBloc = SymptomTypeBloc(null);
-  }
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  closeSearch(BuildContext context) {
-    _symptomTypeBloc.dispose();
-    close(context, null);
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () => closeSearch(context),
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    // if (query.length < 3) {
-    //   return Column(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //     children: <Widget>[
-    //       Center(
-    //         child: Text(
-    //           "Search term must be longer than two letters.",
-    //         ),
-    //       )
-    //     ],
-    //   );
-    // }
-
-    // //Add the search term to the searchBloc.
-    // //The Bloc will then handle the searching and add the results to the searchResults stream.
-    // //This is the equivalent of submitting the search term to whatever search service you are using
-    // InheritedBlocs.of(context)
-    //     .searchBloc
-    //     .searchTerm
-    //     .add(query);
-    _symptomTypeBloc.fetchQuery(query);
-
-    return Column(
-      children: <Widget>[
-        //Build the results based on the searchResults stream in the searchBloc
-        StreamBuilder(
-          stream: _symptomTypeBloc.all,
-          builder: (context, AsyncSnapshot<List<SymptomType>> snapshot) {
-            if (!snapshot.hasData) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Center(child: CircularProgressIndicator()),
-                ],
-              );
-            } else if (snapshot.data.length == 0) {
-              return Column(
-                children: <Widget>[
-                  Text(
-                    "No Results Found.",
-                  ),
-                ],
-              );
-            } else {
-              var results = snapshot.data;
-              return ListView.builder(
-                itemCount: results.length,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  var result = results[index];
-                  return ListTile(
-                      title: Text(result.name),
-                      onTap: () {
-                        this.onSelect(result);
-                        closeSearch(context);
-                      });
-                },
-              );
-            }
-          },
-        ),
-      ],
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    // This method is called everytime the search term changes.
-    // If you want to add search suggestions as the user enters their search term, this is the place to do that.
-    return Column();
-  }
-
-  @override
-  ThemeData appBarTheme(BuildContext context) {
-    assert(context != null);
-    final ThemeData theme = Theme.of(context);
-    assert(theme != null);
-    return theme;
   }
 }
