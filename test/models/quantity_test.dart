9f35a34f375e1e27e88857345b70abe657cf943b
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-21 21:45:34 -0400

    Add quantity tests

---
+++ test/models/quantity_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gut_ai/models/quantity.dart';
+
+void main() {
+  group('Quantity', () {
+    test('constructs empty object', () {
+      Quantity quantity = Quantity();
+      expect(quantity.amount, null);
+      expect(quantity.unit, null);
+    });
+
+    test('constructs simple object', () {
+      Quantity quantity = Quantity(amount: 3.0, unit: 'Cups');
+      expect(quantity.amount, 3.0);
+      expect(quantity.unit, 'Cups');
+    });
+
+    test('is equatable', () {
+      final constructBM = () => Quantity(amount: 3, unit: 'Cups');
+      expect(constructBM(), constructBM());
+    });
+
+    test('is constructable from JSON', () {
+      const Map<String, dynamic> quantityJson = {
+        'amount': 3.0,
+        'unit': 'Cups',
+      };
+      Quantity quantity = Quantity.fromJson(quantityJson);
+      expect(quantity.amount, 3.0);
+      expect(quantity.unit, 'Cups');
+    });
+  });
+}
