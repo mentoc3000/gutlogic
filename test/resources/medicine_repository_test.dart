82d413c4e4be7beb785c98e500f176382c7cd768
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-24 18:30:12 -0400

    Test medicine repository

---
+++ test/resources/medicine_repository_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gut_ai/resources/medicine_repository.dart';
+import 'package:gut_ai/models/medicine.dart';
+
+void main() {
+  group('Medicine Repository', () {
+    test('fetches all medicines', () async {
+      final items = [
+        {"name": "Fiber"},
+        {"name": "Probiotic"}
+      ];
+      MedicineRepository medicineRepository = MedicineRepository();
+      List<Medicine> medicines = await medicineRepository.fetchAll();
+      expect(medicines, items.map((x) => Medicine.fromJson(x)).toList());
+    });
+  });
+}
+
