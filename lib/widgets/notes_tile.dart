4eda37eb690b2ac83178f2b3f86e26ae31bd3b22
JP Sheehan <jp.sheehan2@gmail.com>
2019-05-25 07:17:45 -0400

    Add notes tile

---
+++ lib/widgets/notes_tile.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class NotesTile extends StatefulWidget {
+
+  final String notes;
+
+  NotesTile({this.notes});
+
+  @override
+  _NotesTileState createState() => _NotesTileState();
+}
+
+class _NotesTileState extends State<NotesTile> {
+  TextEditingController _notesController;
+
+  @override
+  void initState() {
+    super.initState();
+    _notesController =
+        TextEditingController(text: widget.notes);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Notes'),
+          TextField(
+            keyboardType: TextInputType.multiline,
+            controller: _notesController,
+            onChanged: (newValue) => widget.notes.replaceRange(0, null, newValue),
+          ),
+        ],
+      ),
+    );
+  }
+}
