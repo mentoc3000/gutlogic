1dd09550c8c70ab5069800d1ac39dea003bf8153
JP Sheehan <jp.sheehan2@gmail.com>
2019-06-02 22:26:14 -0400

    Add diary entries events

--- lib/blocs/diary_entries_event.dart
+++ lib/blocs/diary_entries_event.dart
@@ -1,7 +1,41 @@
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import '../models/diary_entry.dart';
 
 @immutable
 abstract class DiaryEntriesEvent extends Equatable {
   DiaryEntriesEvent([List props = const []]) : super(props);
 }
+
+class LoadDiaryEntries extends DiaryEntriesEvent {
+  @override
+  String toString() => 'LoadDiaryEntries';
+}
+
+class AddDiaryEntry extends DiaryEntriesEvent {
+  final DiaryEntry diaryEntry;
+
+  AddDiaryEntry(this.diaryEntry) : super([diaryEntry]);
+
+  @override
+  String toString() => 'AddDiaryEntry { todo: $diaryEntry }';
+}
+
+class UpdateDiaryEntry extends DiaryEntriesEvent {
+  final DiaryEntry updatedDiaryEntry;
+
+  UpdateDiaryEntry(this.updatedDiaryEntry) : super([updatedDiaryEntry]);
+
+  @override
+  String toString() => 'UpdateDiaryEntry { updatedDiaryEntry: $updatedDiaryEntry }';
+}
+
+class DeleteDiaryEntry extends DiaryEntriesEvent {
+  final DiaryEntry diaryEntry;
+
+  DeleteDiaryEntry(this.diaryEntry) : super([diaryEntry]);
+
+  @override
+  String toString() => 'DeleteDiaryEntry { todo: $diaryEntry }';
+}
+
