37e6862f736c86460937a4c19c4e4ae0debe3174
JP Sheehan <jp.sheehan2@gmail.com>
2019-06-13 20:38:47 -0400

    Extend medicine repository from database repository

--- lib/resources/medicine_repository.dart
+++ lib/resources/medicine_repository.dart
@@ -1,27 +1,13 @@
-import 'dart:async';
+// import 'dart:async';
 import '../models/medicine.dart';
-import '../resources/searchable_repository.dart';
+import 'package:built_collection/built_collection.dart';
+import '../resources/database_repository.dart';
 
-class MedicineRepository implements SearchableRepository {
-
-  final List<Medicine> allMedicines = [
-    Medicine(name: 'Fiber'),
-    Medicine(name: 'Probiotic'),
-  ];
-
-  MedicineRepository();
-
-  Future<List<Medicine>> fetchAll() async {
-    // await Future.delayed(Duration(seconds: 1));
-    return allMedicines;
-  }
-
-  Future<List<Medicine>> fetchQuery(String query) async {
-    if (query == '') {
-      return fetchAll();
-    }
-    return allMedicines
-        .where((f) => f.toString().contains(query))
-        .toList();
+class MedicineRepository extends DatabaseRepository {
+  MedicineRepository() : super() {
+    items = BuiltList([
+      Medicine(name: 'Fiber'),
+      Medicine(name: 'Probiotic'),
+    ]);
   }
 }
