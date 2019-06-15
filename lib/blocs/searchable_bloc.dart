50e4a4ee3b2a612d72f430e16f3635fddaf06df9
JP Sheehan <jp.sheehan2@gmail.com>
2019-06-15 09:30:42 -0400

    Genericize searchable events and states

--- lib/blocs/searchable_bloc.dart
+++ lib/blocs/searchable_bloc.dart
@@ -7,37 +7,39 @@ import '../resources/repository_interfaces.dart';
 import 'searchable_state.dart';
 import 'searchable_event.dart';
 
-abstract class SearchableBloc<T extends Searchable,
-        R extends SearchableRepository>
-    extends Bloc<SearchableEvent, SearchableState> {
+abstract class SearchableBloc<
+    T extends Searchable,
+    R extends SearchableRepository,
+    E extends SearchableEvent,
+    S extends SearchableState> extends Bloc<E, S> {
   @protected
   final R repository;
 
   SearchableBloc(this.repository);
 
   @override
-  SearchableState get initialState => SearchableLoading();
+  S get initialState => SearchableLoading() as S;
 
   @override
-  Stream<SearchableState> mapEventToState(
+  Stream<S> mapEventToState(
     SearchableEvent event,
   ) async* {
     if (event is FetchAll) {
       try {
-        yield SearchableLoading();
+        yield SearchableLoading() as S;
         BuiltList<T> items = await repository.fetchAll();
-        yield SearchableLoaded<T>(items);
+        yield SearchableLoaded<T>(items) as S;
       } catch (_) {
-        yield SearchableError();
+        yield SearchableError() as S;
       }
     }
     if (event is FetchQuery) {
       try {
-        yield SearchableLoading();
+        yield SearchableLoading() as S;
         BuiltList<T> items = await repository.fetchQuery(event.query);
-        yield SearchableLoaded<T>(items);
+        yield SearchableLoaded<T>(items) as S;
       } catch (_) {
-        yield SearchableError();
+        yield SearchableError() as S;
       }
     }
   }
