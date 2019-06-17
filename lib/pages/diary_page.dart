48378d65df78104fdbb048f456be07b80a56e4f7
JP Sheehan <jp.sheehan2@gmail.com>
2019-06-17 07:33:26 -0400

    Refactor DiaryFloatingActionButton

--- lib/pages/diary_page.dart
+++ lib/pages/diary_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import '../widgets/diary_tiles.dart';
 import 'meal_entry_page.dart';
 import '../models/diary_entry.dart';
-import '../blocs/diary_entries_bloc.dart';
+import '../blocs/diary_entry_bloc.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class DiaryPage extends StatefulWidget {
@@ -13,12 +14,10 @@ class DiaryPage extends StatefulWidget {
 }
 
 class DiaryPageState extends State<DiaryPage> {
-  final _diaryEntriesBloc = DiaryEntriesBloc();
-
   @override
   void dispose() {
     super.dispose();
-    _diaryEntriesBloc.dispose();
+    _diaryEntryBloc.dispose();
   }
 
   Widget buildEntryTile(DiaryEntry entry) {
@@ -46,30 +45,31 @@ class DiaryPageState extends State<DiaryPage> {
     }
   }
 
-  List<Widget> entriesToTiles(List<DiaryEntry> entries) {
-
-    if (entries == []) {
+  List<Widget> entryToTiles(List<DiaryEntry> entry) {
+    if (entry == []) {
       return [];
     }
 
-    entries.sort((a, b) => a.dateTime.compareTo(b.dateTime));
+    entry.sort((a, b) => a.dateTime.compareTo(b.dateTime));
     var dateFormatter = DateFormat.yMd();
 
     List<Widget> tiles = [];
-    for (var i = 0; i < entries.length; i++) {
-      if (i == 0 || dateFormatter.format(entries[i].dateTime) != dateFormatter.format(entries[i-1].dateTime)) {
-        tiles.add(DateTile(dateTime: entries[i].dateTime));
+    for (var i = 0; i < entry.length; i++) {
+      if (i == 0 ||
+          dateFormatter.format(entry[i].dateTime) !=
+              dateFormatter.format(entry[i - 1].dateTime)) {
+        tiles.add(DateTile(dateTime: entry[i].dateTime));
       }
-      tiles.add(buildEntryTile(entries[i]));
+      tiles.add(buildEntryTile(entry[i]));
     }
 
     return tiles;
-
   }
 
   @override
-  Widget build (BuildContext context) {
-    _diaryEntriesBloc.fetchAllDiaryEntries();
+  Widget build(BuildContext context) {
+    final diaryEntryBloc = BlocProvider.of<DiaryEntryBloc>(context);
+    _diaryEntryBloc.fetchAllDiaryEntry();
 
     return Scaffold(
         appBar: AppBar(
@@ -81,74 +81,103 @@ class DiaryPageState extends State<DiaryPage> {
             Expanded(
               // child: Container(
               //   decoration: BoxDecoration(border: Border.all()),
-                child: StreamBuilder(
-                  stream: _diaryEntriesBloc.allFoods,
-                  builder: (context, AsyncSnapshot<List<DiaryEntry>> snapshot) {
-                    if (!snapshot.hasData) {
-                      return Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Center(child: CircularProgressIndicator()),
-                          ],);
-                    } else {
-                      List<Widget> tiles = entriesToTiles(snapshot.data);
-                      return ListView.separated(
-                        separatorBuilder: (context, index) => Divider(),
-                        itemCount: tiles.length,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) => Padding(
-                              padding: EdgeInsets.all(0.0),
-                              child: tiles[index],
-                            ),
-                        padding: EdgeInsets.all(0.0),
-                      );
-                    }
-                  },
-                ),
+              child: StreamBuilder(
+                stream: _diaryEntryBloc.allFoods,
+                builder: (context, AsyncSnapshot<List<DiaryEntry>> snapshot) {
+                  if (!snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(child: CircularProgressIndicator()),
+                      ],
+                    );
+                  } else {
+                    List<Widget> tiles = entryToTiles(snapshot.data);
+                    return ListView.separated(
+                      separatorBuilder: (context, index) => Divider(),
+                      itemCount: tiles.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) => Padding(
+                            padding: EdgeInsets.all(0.0),
+                            child: tiles[index],
+                          ),
+                      padding: EdgeInsets.all(0.0),
+                    );
+                  }
+                },
+              ),
               // ),
             ),
           ],
         ),
-        floatingActionButton: SpeedDial(
-          animatedIcon: AnimatedIcons.menu_close,
-          // child: Icon(Icons.add),
-          children: [
-            SpeedDialChild(
-                child: Icon(Icons.fastfood),
-                backgroundColor: Colors.blue,
-                label: 'Food & Drink',
-                onTap: () {
-                  MealEntry newMeal = MealEntry.newEntry();
-                  _diaryEntriesBloc.insert(newMeal);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MealEntryPage(
-                                entry: newMeal,
-                                onUpdate: (_) =>
-                                    _diaryEntriesBloc.fetchAllDiaryEntries(),
-                              ),),);
-                },),
-            SpeedDialChild(
-              child: Icon(Icons.arrow_drop_up),
-              backgroundColor: Colors.purple,
-              label: 'Bowel Movement',
-              onTap: () => print('SECOND CHILD'),
-            ),
-            SpeedDialChild(
-              child: Icon(Icons.face),
-              backgroundColor: Colors.red,
-              label: 'Symptom',
-              onTap: () => print('THIRD CHILD'),
-            ),
-            SpeedDialChild(
-              child: Icon(Icons.pause_circle_outline),
-              backgroundColor: Colors.orange,
-              label: 'Medicine',
-              onTap: () => print('THIRD CHILD'),
-            ),
-          ],
-        ),);
+        floatingActionButton: DiaryFloatingActionButton(
+          newMealTapped: () {
+            MealEntry newMeal = MealEntry.newEntry();
+            _diaryEntryBloc.insert(newMeal);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MealEntryPage(
+                      entry: newMeal,
+                      onUpdate: (_) => _diaryEntryBloc.fetchAllDiaryEntry(),
+                    ),
+              ),
+            );
+          },
+          newBowelMovementTapped: () => print('SECOND CHILD'),
+          newMedicinesTapped: () => print('THIRD CHILD'),
+          newSymptomTapped: () => print('FOURTH CHILD'),
+        ));
+  }
+}
+
+typedef _Action = void Function();
+
+class DiaryFloatingActionButton extends StatelessWidget {
+  final _Action newMealTapped;
+  final _Action newBowelMovementTapped;
+  final _Action newMedicinesTapped;
+  final _Action newSymptomTapped;
+
+  DiaryFloatingActionButton({
+    this.newMealTapped,
+    this.newBowelMovementTapped,
+    this.newMedicinesTapped,
+    this.newSymptomTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      // child: Icon(Icons.add),
+      children: [
+        SpeedDialChild(
+          child: Icon(Icons.fastfood),
+          backgroundColor: Colors.blue,
+          label: 'Food & Drink',
+          onTap: newMealTapped,
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.arrow_drop_up),
+          backgroundColor: Colors.purple,
+          label: 'Bowel Movement',
+          onTap: newBowelMovementTapped,
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.face),
+          backgroundColor: Colors.red,
+          label: 'Symptom',
+          onTap: newMedicinesTapped,
+        ),
+        SpeedDialChild(
+          child: Icon(Icons.pause_circle_outline),
+          backgroundColor: Colors.orange,
+          label: 'Medicine',
+          onTap: newSymptomTapped,
+        ),
+      ],
+    );
   }
 }
