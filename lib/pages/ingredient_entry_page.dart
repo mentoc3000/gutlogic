2e8b916a92625f3e4723d6bee579448986405289
JP Sheehan <jp.sheehan2@gmail.com>
2019-06-22 08:41:20 -0400

    Show ingredient

--- lib/pages/ingredient_entry_page.dart
+++ lib/pages/ingredient_entry_page.dart
@@ -1,86 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../models/ingredient.dart';
+import '../models/food.dart';
+import '../models/quantity.dart';
 import '../widgets/quantity_view.dart';
 import '../blocs/food_bloc.dart';
 import '../resources/app_sync_service.dart';
 import '../resources/user_service.dart';
 import 'gutai_search_delegate.dart';
 import '../widgets/placeholder_widget.dart';
+import '../blocs/diary_entry_bloc.dart';
+import '../blocs/database_event.dart';
 
 class IngredientEntryPage extends StatefulWidget {
   static String tag = 'ingredient-entry-page';
 
   final Ingredient ingredient;
+  final void Function(Ingredient) onSave;
 
-  IngredientEntryPage({this.ingredient});
+  IngredientEntryPage({this.ingredient, this.onSave});
 
   @override
   IngredientEntryPageState createState() => IngredientEntryPageState();
 }
 
 class IngredientEntryPageState extends State<IngredientEntryPage> {
+  Food _food;
+  Quantity _quantity;
+
+  @override
+  void initState() {
+    super.initState();
+    _food = widget.ingredient.food ?? null;
+    _quantity = widget.ingredient.quantity ?? null;
+  }
+
+  List<Widget> buildItems() {
+    return [
+      QuantityView(
+        quantity: _quantity,
+      ),
+    ];
+  }
+
+  void showFoodSearch(BuildContext context) {
+    FoodBloc foodBloc = BlocProvider.of<FoodBloc>(context);
+
+    final onSelect = (food) => this._food = food;
+
+    showSearch(
+      context: context,
+      delegate: GutAiSearchDelegate(
+        searchableBloc: foodBloc,
+        onSelect: onSelect,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return PlaceholderWidget(Colors.lightBlue);
+    List<Widget> items = buildItems();
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(_food?.name ?? 'Ingredient'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => showFoodSearch(context),
+          ),
+        ],
+      ),
+      body: Form(
+        child: ListView.separated(
+          separatorBuilder: (context, index) => Divider(),
+          itemCount: items.length,
+          itemBuilder: (context, index) =>
+              Padding(padding: EdgeInsets.all(1.0), child: items[index]),
+          padding: EdgeInsets.all(0.0),
+        ),
+      ),
+    );
   }
-  // Ingredient _ingredient;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _ingredient = widget.ingredient;
-  // }
-
-  // List<Widget> buildItems() {
-  //   return [
-  //     QuantityView(
-  //       quantity: _ingredient.quantity,
-  //     ),
-  //   ];
-  // }
-
-  // void showFoodSearch(BuildContext context) {
-  //   final userService = UserService();
-  //   final session = userService.getSession();
-  //   AppSyncService appSyncService = AppSyncService(session);
-  //   FoodBloc foodBloc = FoodBloc(appSyncService);
-
-  //   final onSelect = (food) => this._ingredient.food = food;
-
-  //   showSearch(
-  //     context: context,
-  //     delegate: GutAiSearchDelegate(
-  //       searchableBloc: foodBloc,
-  //       onSelect: onSelect,
-  //     ),
-  //   );
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   List<Widget> items = buildItems();
-
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       centerTitle: true,
-  //       title: Text(_ingredient?.food?.name ?? 'Ingredient'),
-  //       actions: <Widget>[
-  //         IconButton(
-  //           icon: Icon(Icons.search),
-  //           onPressed: () => showFoodSearch(context),
-  //         ),
-  //       ],
-  //     ),
-  //     body: Form(
-  //       child: ListView.separated(
-  //         separatorBuilder: (context, index) => Divider(),
-  //         itemCount: items.length,
-  //         itemBuilder: (context, index) =>
-  //             Padding(padding: EdgeInsets.all(1.0), child: items[index]),
-  //         padding: EdgeInsets.all(0.0),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
