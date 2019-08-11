5d8520349e0012b02b61c50b4105c53ece872737
JP Sheehan <jp.sheehan2@gmail.com>
2019-08-11 12:25:48 -0400

    Add chai

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
-var assert = require('assert');
+const chai = require('chai');
+
+var expect = chai.expect;
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      expect([1, 2, 3].indexOf(4)).to.equal(-1);
     });
   });
 });
