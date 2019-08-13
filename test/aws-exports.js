2ce2b38f4bc4bf988d8ce22631ebeb318f323494
JP Sheehan <jp.sheehan2@gmail.com>
2019-08-13 06:44:29 -0400

    Move boilerplate to aws-exports

--- test/aws-exports.js
+++ test/aws-exports.js
@@ -10,3 +10,75 @@ var config = {
 };
 config.ENDPOINT = "https://" + config.HOST + config.PATH;
 exports.default = config;
+
+global.WebSocket = require('ws');
+global.window = global.window || {
+    setTimeout: setTimeout,
+    clearTimeout: clearTimeout,
+    WebSocket: global.WebSocket,
+    ArrayBuffer: global.ArrayBuffer,
+    addEventListener: function () { },
+    navigator: { onLine: true }
+};
+global.localStorage = {
+    store: {},
+    getItem: function (key) {
+        return this.store[key]
+    },
+    setItem: function (key, value) {
+        this.store[key] = value
+    },
+    removeItem: function (key) {
+        delete this.store[key]
+    }
+};
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+// Require AppSync module
+const AUTH_TYPE = require('aws-appsync/lib/link/auth-link').AUTH_TYPE;
+const AWSAppSyncClient = require('aws-appsync').default;
+
+const url = config.ENDPOINT;
+const region = config.REGION;
+// const type = AUTH_TYPE.AWS_IAM;
+const type = AUTH_TYPE.API_KEY
+// const type = AUTH_TYPE.AMAZON_COGNITO_USER_POOLS
+
+// If you want to use API key-based auth
+const apiKey = 'da2-dq2yiriyi5aa3du5nlineuwjom';
+// If you want to use a jwtToken from Amazon Cognito identity:
+const jwtToken = 'xxxxxxxx';
+
+// If you want to use AWS...
+const AWS = require('aws-sdk');
+const credentials = AWS.config.credentials;
+
+
+// Set up Apollo client
+const client = new AWSAppSyncClient(
+    {
+        url: url,
+        region: region,
+        auth: {
+            type: type,
+            // credentials: credentials,
+            apiKey: apiKey,
+        },
+        disableOffline: true,
+    },     //Uncomment for AWS Lambda
+    // {
+    //     defaultOptions: {
+    //         query: {
+    //             fetchPolicy: 'network-only',
+    //             errorPolicy: 'all',
+    //         },
+    //         mutate: {
+    //             fetchPolicy: 'no-cache',
+    //             errorPolicy: 'all',
+    //         }
+    //     },
+    // }
+);
+
+exports.client = client;
--- test/food.spec.js
+++ test/food.spec.js
@@ -4,78 +4,8 @@ chai.use(assertArrays);
 
 var expect = chai.expect;
 
-global.WebSocket = require('ws');
-global.window = global.window || {
-    setTimeout: setTimeout,
-    clearTimeout: clearTimeout,
-    WebSocket: global.WebSocket,
-    ArrayBuffer: global.ArrayBuffer,
-    addEventListener: function () { },
-    navigator: { onLine: true }
-};
-global.localStorage = {
-    store: {},
-    getItem: function (key) {
-        return this.store[key]
-    },
-    setItem: function (key, value) {
-        this.store[key] = value
-    },
-    removeItem: function (key) {
-        delete this.store[key]
-    }
-};
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
-
 // Require exports file with endpoint and auth info
-const aws_exports = require('./aws-exports').default;
-
-// Require AppSync module
-const AUTH_TYPE = require('aws-appsync/lib/link/auth-link').AUTH_TYPE;
-const AWSAppSyncClient = require('aws-appsync').default;
-
-const url = aws_exports.ENDPOINT;
-const region = aws_exports.REGION;
-// const type = AUTH_TYPE.AWS_IAM;
-const type = AUTH_TYPE.API_KEY
-// const type = AUTH_TYPE.AMAZON_COGNITO_USER_POOLS
-
-// If you want to use API key-based auth
-const apiKey = 'da2-dq2yiriyi5aa3du5nlineuwjom';
-// If you want to use a jwtToken from Amazon Cognito identity:
-const jwtToken = 'xxxxxxxx';
-
-// If you want to use AWS...
-const AWS = require('aws-sdk');
-const credentials = AWS.config.credentials;
-
-
-// Set up Apollo client
-const client = new AWSAppSyncClient(
-    {
-        url: url,
-        region: region,
-        auth: {
-            type: type,
-            // credentials: credentials,
-            apiKey: apiKey,
-        },
-        disableOffline: true,
-    },     //Uncomment for AWS Lambda
-    // {
-    //     defaultOptions: {
-    //         query: {
-    //             fetchPolicy: 'network-only',
-    //             errorPolicy: 'all',
-    //         },
-    //         mutate: {
-    //             fetchPolicy: 'no-cache',
-    //             errorPolicy: 'all',
-    //         }
-    //     },
-    // }
-);
+const client = require('./aws-exports').client;
 
 // Import gql helper and craft a GraphQL query
 const gql = require('graphql-tag');
