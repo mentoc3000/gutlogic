81b390a32aaaf4a8b553ddbc3dc5086481b57600
JP Sheehan <jp.sheehan2@gmail.com>
2019-10-06 19:48:03 -0400

    Reformat diary entry tests

--- test/diaryEntry.spec.js
+++ test/diaryEntry.spec.js
@@ -1,33 +1,35 @@
+const { API, graphqlOperation } = require('aws-amplify');
 const chai = require('chai');
 const gql = require('graphql-tag');
 const assertArrays = require('chai-arrays');
-const { client } = require('./aws-exports');
+const { config, signIn, signOut } = require('./aws-setup');
 const dummyDb = require('./dummyDb');
+
+API.configure(config);
 
 chai.use(assertArrays);
 const { expect } = chai;
 
-describe('DiaryEntry database', () => {
-  const userId = 'fakeuserid';
-
+describe('DiaryEntry database', function diaryEntryTests() {
+  this.timeout(5000);
+  before('Sign in', signIn);
+  after('Sign out', async () => {
+    await dummyDb.clearItems();
+    await signOut();
+  });
   describe('listDiaryEntries', () => {
     it('should get all diary entries', async () => {
       const query = gql(`
-        query ListDiaryEntries($userId: String!) {
-        listDiaryEntries(userId: $userId) {
+        query ListDiaryEntries {
+        listDiaryEntries {
             items {
               id
             }
         }
         }`);
 
-      await client.hydrated();
-      const result = await client.query({
-        query,
-        variables: { userId },
-      });
+      const result = await API.graphql(graphqlOperation(query));
       const data = result.data.listDiaryEntries;
-      expect(data.__typename).to.equal('PaginatedDiaryEntries');
       expect(data.items).to.be.array();
     });
   });
@@ -38,30 +40,26 @@ describe('DiaryEntry database', () => {
       const mutation = gql(`
         mutation CreateDiaryEntry($input: CreateDiaryEntryInput!) {
         createDiaryEntry(input: $input) {
-            nameId
-            entryId
+          id
+          type
             meal { ingredients { entryId }}
         }
         }`);
 
-      await client.hydrated();
-      const result = await client.mutate({
-        mutation,
-        variables: {
+      const result = await API.graphql(
+        graphqlOperation(mutation, {
           input: {
-            userId,
+            type: 'MEAL',
             creationDate: datetime,
             modificationDate: datetime,
             datetime,
-            meal: {
-              ingredients: [],
-            },
           },
-        },
-      });
+        })
+      );
       const data = result.data.createDiaryEntry;
-      expect(data.__typename).to.equal('DiaryEntry');
-      expect(data.meal.ingredients).to.be.array();
+      expect(data.type).to.equal('MEAL');
+      expect(data.ingredients).to.be.array();
+      expect(data.ingredients.length).to.equal(0);
     });
 
     it('should create a bowel movement entry', async () => {
@@ -69,18 +67,16 @@ describe('DiaryEntry database', () => {
       const mutation = gql(`
         mutation CreateDiaryEntry($input: CreateDiaryEntryInput!) {
         createDiaryEntry(input: $input) {
-            nameId
-            entryId
+          id
+          type
             bowelMovement { volume }
         }
         }`);
 
-      await client.hydrated();
-      const result = await client.mutate({
-        mutation,
-        variables: {
+      const result = await API.graphql(
+        graphqlOperation(mutation, {
           input: {
-            userId,
+            type: 'BOWELMOVEMENT',
             creationDate: datetime,
             modificationDate: datetime,
             datetime,
@@ -89,10 +85,10 @@ describe('DiaryEntry database', () => {
               volume: 4,
             },
           },
-        },
-      });
+        })
+      );
       const data = result.data.createDiaryEntry;
-      expect(data.__typename).to.equal('DiaryEntry');
+      expect(data.type).to.equal('BOWELMOVEMENT');
       expect(data.bowelMovement.volume).to.equal(4);
     });
   });
@@ -104,30 +100,27 @@ describe('DiaryEntry database', () => {
       const mutation = gql(`
         mutation CreateMealEntry($input: CreateMealEntryInput!) {
         createMealEntry(input: $input) {
-            nameId
-            entryId
-            creationDate
-            modificationDate
-            datetime
-            meal { ingredients { entryId }}
+          id
+          type
+          creationDate
+          modificationDate
+          datetime
+          meal { ingredients { id }}
         }
         }`);
 
-      await client.hydrated();
-      const result = await client.mutate({
-        mutation,
-        variables: {
+      const result = await API.graphql(
+        graphqlOperation(mutation, {
           input: {
-            userId,
             creationDate: creationDatetime,
             datetime,
           },
-        },
-      });
+        })
+      );
       const data = result.data.createMealEntry;
-      expect(data.__typename).to.equal('DiaryEntry');
-      expect(data.meal.ingredients).to.be.array();
-      expect(data.meal.ingredients.length).to.equal(0);
+      expect(data.type).to.equal('MEAL');
+      expect(data.ingredients).to.be.array();
+      expect(data.ingredients.length).to.equal(0);
       expect(data.datetime).to.equal(datetime);
       expect(data.creationDate).to.equal(creationDatetime);
       expect(data.modificationDate).to.equal(data.creationDate);
@@ -141,8 +134,8 @@ describe('DiaryEntry database', () => {
       const mutation = gql(`
         mutation CreateBowelMovementEntry($input: CreateBowelMovementEntryInput!) {
         createBowelMovementEntry(input: $input) {
-            nameId
-            entryId
+          id
+          type
             creationDate
             modificationDate
             datetime
@@ -150,12 +143,9 @@ describe('DiaryEntry database', () => {
         }
         }`);
 
-      await client.hydrated();
-      const result = await client.mutate({
-        mutation,
-        variables: {
+      const result = await API.graphql(
+        graphqlOperation(mutation, {
           input: {
-            userId,
             creationDate: creationDatetime,
             datetime,
             bowelMovement: {
@@ -163,10 +153,10 @@ describe('DiaryEntry database', () => {
               volume: 4,
             },
           },
-        },
-      });
+        })
+      );
       const data = result.data.createBowelMovementEntry;
-      expect(data.__typename).to.equal('DiaryEntry');
+      expect(data.type).to.equal('BOWELMOVEMENT');
       expect(data.bowelMovement.type).to.equal(3);
       expect(data.bowelMovement.volume).to.equal(4);
       expect(data.datetime).to.equal(datetime);
@@ -182,8 +172,8 @@ describe('DiaryEntry database', () => {
       const mutation = gql(`
         mutation CreateDosageEntry($input: CreateDosageEntryInput!) {
         createDosageEntry(input: $input) {
-            nameId
-            entryId
+          id
+          type
             creationDate
             modificationDate
             datetime
@@ -191,20 +181,18 @@ describe('DiaryEntry database', () => {
         }
         }`);
 
-      await client.hydrated();
-      const result = await client.mutate({
-        mutation,
-        variables: {
+      const result = await API.graphql(
+        graphqlOperation(mutation, {
           input: {
-            userId,
             creationDate: creationDatetime,
             datetime,
           },
-        },
-      });
+        })
+      );
       const data = result.data.createDosageEntry;
-      expect(data.__typename).to.equal('DiaryEntry');
-      expect(data.dosage.doses).to.be.array();
+      expect(data.type).to.equal('DOSAGE');
+      expect(data.doses).to.be.array();
+      expect(data.doses.length).to.equal(0);
       expect(data.datetime).to.equal(datetime);
       expect(data.creationDate).to.equal(creationDatetime);
       expect(data.modificationDate).to.equal(data.creationDate);
@@ -218,8 +206,8 @@ describe('DiaryEntry database', () => {
       const mutation = gql(`
         mutation CreateSymptomEntry($input: CreateSymptomEntryInput!) {
         createSymptomEntry(input: $input) {
-            nameId
-            entryId
+          id
+          type
             creationDate
             modificationDate
             datetime
@@ -230,12 +218,9 @@ describe('DiaryEntry database', () => {
         }
         }`);
 
-      await client.hydrated();
-      const result = await client.mutate({
-        mutation,
-        variables: {
+      const result = await API.graphql(
+        graphqlOperation(mutation, {
           input: {
-            userId,
             creationDate: creationDatetime,
             datetime,
             symptom: {
@@ -243,10 +228,10 @@ describe('DiaryEntry database', () => {
               severity: 4.3,
             },
           },
-        },
-      });
+        })
+      );
       const data = result.data.createSymptomEntry;
-      expect(data.__typename).to.equal('DiaryEntry');
+      expect(data.type).to.equal('SYMPTOM');
       expect(data.datetime).to.equal(datetime);
       expect(data.creationDate).to.equal(creationDatetime);
       expect(data.modificationDate).to.equal(data.creationDate);
@@ -260,24 +245,23 @@ describe('DiaryEntry database', () => {
     const datetime = '2019-07-02T12:43:00Z';
 
     before('create a diary entry', async () => {
-      id = await dummyDb.createMealEntry(userId, datetime);
+      id = await dummyDb.createMealEntry(datetime);
     });
 
     it('should get a diary entry', async () => {
       const getDiaryEntry = gql(`
-        query getDiaryEntry($nameId: String!, $entryId: String!) {
-        getDiaryEntry(nameId: $nameId, entryId: $entryId) {
-            nameId
-            entryId
+        query getDiaryEntry($id: ID!) {
+        getDiaryEntry(id: $id) {
+          id
+          type
         }
         }`);
 
-      const getResult = await client.query({
-        query: getDiaryEntry,
-        variables: id,
-      });
+      const getResult = await API.graphql(
+        graphqlOperation(getDiaryEntry, { id })
+      );
       const getData = getResult.data.getDiaryEntry;
-      expect(getData.__typename).to.equal('DiaryEntry');
+      expect(getData.type).to.equal('MEAL');
     });
   });
 
@@ -286,25 +270,22 @@ describe('DiaryEntry database', () => {
     const datetime = '2019-07-02T12:43:00Z';
 
     beforeEach('create a diary entry', async () => {
-      id = await dummyDb.createMealEntry(userId, datetime);
+      id = await dummyDb.createMealEntry(datetime);
     });
 
     it('should delete a diary entry', async () => {
       const deleteDiaryEntry = gql(`
-        mutation DeleteDiaryEntry($input: GutAiIdInput!) {
+        mutation DeleteDiaryEntry($input: DeleteDiaryEntryInput!) {
         deleteDiaryEntry(input: $input) {
-            nameId
-            entryId
+          id
         }
         }`);
 
-      await client.hydrated();
-      const result = await client.mutate({
-        mutation: deleteDiaryEntry,
-        variables: {
+      const result = await API.graphql(
+        graphqlOperation(deleteDiaryEntry, {
           input: id,
-        },
-      });
+        })
+      );
       const data = result.data.deleteDiaryEntry;
       expect(data.__typename).to.equal('DiaryEntry');
       expect(data.nameId).to.equal(id.nameId);
@@ -317,37 +298,33 @@ describe('DiaryEntry database', () => {
     const datetime = '2019-07-02T12:43:00Z';
 
     beforeEach('create a meal entry', async () => {
-      id = await dummyDb.createMealEntry(userId, datetime);
+      id = await dummyDb.createMealEntry(datetime);
     });
 
     it('should update a meal entry', async () => {
       const updateDiaryEntry = gql(`
         mutation UpdateDiaryEntry($input: UpdateDiaryEntryInput!) {
         updateDiaryEntry(input: $input) {
-            nameId
-            entryId
+          id
             modificationDate
             datetime
         }
         }`);
 
       const newDatetime = '2019-08-02T12:43:00Z';
-      const result = await client.mutate({
-        mutation: updateDiaryEntry,
-        variables: {
+      const result = await API.graphql(
+        graphqlOperation(updateDiaryEntry, {
           input: {
             nameId: id.nameId,
             entryId: id.entryId,
             datetime: newDatetime,
             modificationDate: newDatetime,
           },
-        },
-      });
+        })
+      );
       const data = result.data.updateDiaryEntry;
       expect(data.datetime).to.equal(newDatetime);
       expect(data.modificationDate).to.equal(newDatetime);
     });
   });
-
-  after('clear diaryentry database', dummyDb.clearItems);
 });
