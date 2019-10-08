828acb6fe4e76263e34a0f93325c4f0127d45be0
JP Sheehan <jp.sheehan2@gmail.com>
2019-10-08 07:38:20 -0400

    Simplify authentication bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -50,30 +50,19 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  AuthenticationBloc _authenticationBloc;
   UserRepository get _userRepository => widget.userRepository;
 
   @override
-  void initState() {
-    // _userRepository.init();
-    _authenticationBloc = AuthenticationBloc(userRepository: _userRepository);
-    _authenticationBloc.dispatch(AppStarted());
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _authenticationBloc.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BlocProvider<AuthenticationBloc>.value(
-      value: _authenticationBloc,
+    return BlocProvider<AuthenticationBloc>(
+      builder: (BuildContext context) {
+        AuthenticationBloc _authenticationBloc =
+            AuthenticationBloc(userRepository: _userRepository);
+        _authenticationBloc.dispatch(AppStarted());
+        return _authenticationBloc;
+      },
       child: MaterialApp(
         home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-          bloc: _authenticationBloc,
           builder: (BuildContext context, AuthenticationState state) {
             if (state is AuthenticationUninitialized) {
               return SplashPage();
