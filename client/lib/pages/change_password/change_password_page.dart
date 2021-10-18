1ea3b0429644fedeb11d6eac86fc9e1b3bdb1369
JP Sheehan <jp@gutlogic.co>
2021-10-17 20:23:50 -0400

    Change password responds to state

--- client/lib/pages/change_password/change_password_page.dart
+++ client/lib/pages/change_password/change_password_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../blocs/change_password/change_password.dart';
 import '../../widgets/gl_app_bar.dart';
 import '../../widgets/gl_scaffold.dart';
-import '../../widgets/snack_bars/error_snack_bar.dart';
 import 'widgets/change_password_form.dart';
 
 class ChangePasswordPage extends StatelessWidget {
@@ -28,13 +27,5 @@ class ChangePasswordPage extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  void listener(BuildContext context, ChangePasswordState state) {
-    if (state is ChangePasswordError) {
-      ScaffoldMessenger.of(context).showSnackBar(ErrorSnackBar(text: state.message));
-    } else if (state is ChangePasswordSuccess) {
-      Navigator.of(context).pop();
-    }
   }
 }
--- client/lib/pages/change_password/widgets/change_password_form.dart
+++ client/lib/pages/change_password/widgets/change_password_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../auth/auth_provider.dart';
 import '../../../blocs/change_password/change_password.dart';
+import '../../../widgets/snack_bars/error_snack_bar.dart';
 import '../models/change_password_input_group.dart';
 import 'change_password_form_widgets.dart';
 import 'change_password_upgrade_warning.dart';
@@ -10,13 +11,15 @@ import 'change_password_upgrade_warning.dart';
 class ChangePasswordForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return BlocConsumer<ChangePasswordBloc, ChangePasswordState>(builder: builder, listener: listener);
+  }
+
+  Widget builder(BuildContext context, ChangePasswordState state) {
     final user = context.select((ChangePasswordBloc bloc) => bloc.state.user);
     final hasPasswordProvider = user.providers.contains(AuthProvider.password);
 
     return BlocProvider(
-      create: (context) {
-        return ChangePasswordInputGroup();
-      },
+      create: (context) => ChangePasswordInputGroup(),
       child: SingleChildScrollView(
         child: Column(
           children: [
@@ -29,5 +32,13 @@ class ChangePasswordForm extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void listener(BuildContext context, ChangePasswordState state) {
+    if (state is ChangePasswordError) {
+      ScaffoldMessenger.of(context).showSnackBar(ErrorSnackBar(text: state.message));
+    } else if (state is ChangePasswordSuccess) {
+      Navigator.of(context).pop();
+    }
   }
 }
