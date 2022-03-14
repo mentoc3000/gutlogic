d564aa569eaaf7df324e0ede879b29eb260efafc
Chris Parker <chris@gutlogic.co>
2022-03-14 14:16:13 -0400

    Export initialized fft as default export

--- server/functions/test/fft.js
+++ server/functions/test/fft.js
@@ -1,3 +1,3 @@
-exports.fft = require('firebase-functions-test')({
+module.exports = require('firebase-functions-test')({
   projectId: 'gutlogic-dev'
 }, process.env.GOOGLE_APPLICATION_CREDENTIALS);
