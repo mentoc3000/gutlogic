eca77bf8cd203937919b6cf6703723caf961b426
Chris Parker <chris@gutlogic.co>
2022-04-12 16:04:11 -0400

    Cherrypick release/1.2 changes, improve update_data script

--- server/bin/update_data.js
+++ server/bin/update_data.js
@@ -1,120 +1,69 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
 const admin = require('firebase-admin');
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3').verbose();
 
-const foodIrritantDbPath = path.resolve(__dirname, '../data', 'irritants.sqlite3');
+// Returns a new multi-map (each key is a list of values) using the key returned by the [keyfunc] function and the value returned by the [valfunc] function.
+function multimap(collection, keyfunc, valfunc) {
+  const map = new Map();
 
-const maxBatchSize = 500;
+  for (const el of collection) {
+    const key = keyfunc(el);
+    const val = valfunc(el);
+    map.set(key, [...map.get(key) ?? [], val]);
+  }
 
-admin.initializeApp();
+  return map;
+}
 
-const firestore = admin.firestore();
-const foodIrritantsCollection = firestore.collection('food_irritants');
-const foodGroupsCollection = firestore.collection('food_groups');
-
+// Returns a new multi-map of edamam ID values keyed by food ID.
 async function selectEdamamIdMap(db) {
-  return new Promise((resolve, reject) => {
-    // TODO: could be more efficient with GROUP_CONCAT followed by splitting on comma
-    const sql = `SELECT food_id, edamam_id FROM edamam`;
-    db.all(sql, [], (err, rows) => {
-      if (err) {
-        reject(err);
-      }
-      const data = new Map();
-      for (const row of rows) {
-        const foodId = row.food_id;
-        const edamamId = row.edamam_id;
-        let edamamIds = data.get(foodId);
-        if (edamamIds === undefined) {
-          edamamIds = [];
-        }
-        edamamIds.push(edamamId);
-        data.set(foodId, edamamIds);
-      }
-      resolve(data);
-    });
-  });
+  const sql = `SELECT food_id, edamam_id FROM edamam`;
+
+  const keyfunc = (row) => row.food_id;
+  const valfunc = (row) => row.edamam_id;
+
+  return multimap(await db.all(sql), keyfunc, valfunc);
 }
 
+// Returns a new multi-map of food name values keyed by food ID.
 async function selectFoodNameMap(db) {
-  return new Promise((resolve, reject) => {
-    const sql = `SELECT food_id, food_name FROM food_names`;
-    db.all(sql, [], (err, rows) => {
-      if (err) {
-        reject(err);
-      }
-      const data = new Map();
-      for (const row of rows) {
-        const foodId = row.food_id;
-        const foodName = row.food_name;
-        let foodNames = data.get(foodId);
-        if (foodNames === undefined) {
-          foodNames = [];
-        }
-        foodNames.push(foodName);
-        data.set(foodId, foodNames);
-      }
-      resolve(data);
-    });
-  });
+  const sql = `SELECT food_id, food_name FROM food_names`;
+
+  const keyfunc = (row) => row.food_id;
+  const valfunc = (row) => row.food_name;
+
+  return multimap(await db.all(sql), keyfunc, valfunc);
 }
 
+// Returns a new multi-map of { name, concentration, dosePerServing } values keyed by food ID.
 async function selectIrritantMap(db) {
-  return new Promise((resolve, reject) => {
-    // TODO: get max of each irritant
-    const sql = `
+  const sql = `
 SELECT
-    foods.food_id as food_id,
-    irritant_name as irritant,
-    MAX(concentration) as concentration,
-    foods.weight_per_serving as weight_per_serving
+  foods.food_id as food_id,
+  irritant_name as irritant,
+  MAX(concentration) as concentration,
+  foods.weight_per_serving as weight_per_serving
 FROM
-    irritant_content
+  irritant_content
 INNER JOIN foods ON irritant_content.food_id = foods.food_id
 GROUP BY irritant_content.food_id, irritant`;
-    db.all(sql, [], (err, rows) => {
-      if (err) {
-        reject(err);
-      }
-      const data = new Map();
-      for (const row of rows) {
-        const foodId = row.food_id;
-        const name = row.irritant;
-        const concentration = row.concentration;
-        const weightPerServing = row.weight_per_serving;
-        const dosePerServing = concentration * weightPerServing;
-        const irritant = { concentration, dosePerServing, name };
-        let irritants = data.get(foodId);
-        if (irritants === undefined) {
-          irritants = [];
-        }
-        irritants.push(irritant);
-        data.set(foodId, irritants);
-      }
-      resolve(data);
-    });
-  });
+
+  const keyfunc = (row) => row.food_id;
+  const valfunc = (row) => ({ name: row.irritant, concentration: row.concentration, dosePerServing: row.concentration * row.weight_per_serving });
+
+  return multimap(await db.all(sql), keyfunc, valfunc);
 }
 
 /// Return the irritant with the maximum concentration with name matching [name].
 /// If no elements match [name], return null.
 function maxConIrritant(irritants, name) {
-  return irritants.reduce(
-    (acc, el) => {
-      if (el.name === name) {
-        if (acc === null) {
-          return el;
-        } else {
-          return el.concentration > acc.concentration ? el : acc;
-        }
-      } else {
-        return acc;
-      }
-    },
-    null
-  );
+  return irritants
+    .filter((el) => el.name === name)
+    .reduce((acc, el) => acc === null || el.concentration > acc.concentration ? el : acc, null);
 }
 
 function processIrritants(irritants) {
@@ -124,12 +73,11 @@ function processIrritants(irritants) {
   const fructose = maxConIrritant(irritants, 'Fructose');
   const glucose = maxConIrritant(irritants, 'Glucose');
   if (fructose !== null && glucose !== null) {
-    const excessFructose = {
+    processedIrritants.push({
       name: 'Fructose',
       concentration: Math.max(fructose.concentration - glucose.concentration, 0),
       dosePerServing: Math.max(fructose.dosePerServing - glucose.dosePerServing, 0),
-    };
-    processedIrritants.push(excessFructose);
+    });
   }
 
   // Disaccharides
@@ -154,12 +102,11 @@ function processIrritants(irritants) {
   const raffinose = maxConIrritant(irritants, 'Raffinose');
   const stachyose = maxConIrritant(irritants, 'Stachyose');
   if (raffinose !== null || stachyose !== null) {
-    const gos = {
+    processedIrritants.push({
       name: 'GOS',
       concentration: raffinose.concentration + stachyose.concentration,
       dosePerServing: raffinose.dosePerServing + stachyose.dosePerServing,
-    };
-    processedIrritants.push(gos);  
+    });
   }
 
   // Fructo-oligosaccharides (fructan)
@@ -168,7 +115,7 @@ function processIrritants(irritants) {
   const nystose = maxConIrritant(irritants, 'Nystose');
   const kestose = maxConIrritant(irritants, 'Kestose');
   const totalFructan = maxConIrritant(irritants, 'Fructan')
-  
+
   const concentrationNystose = nystose !== null ? nystose.concentration : null;
   const concentrationKestose = kestose !== null ? kestose.concentration : null;
   const concentrationTotalFructan = totalFructan !== null ? totalFructan.concentration : null;
@@ -176,15 +123,14 @@ function processIrritants(irritants) {
   if (totalFructan === null && nystose === null && kestose === null) {
     // pass
   } else if (totalFructan === null || concentrationTotalFructan < concentrationNystose + concentrationKestose) {
-    const fructan = {
+    processedIrritants.push({
       name: 'Fructan',
       concentration: nystose.concentration + kestose.concentration,
       dosePerServing: nystose.dosePerServing + kestose.dosePerServing,
-    };
-    processedIrritants.push(fructan);
+    });
   } else {
     processedIrritants.push(totalFructan);
-  }  
+  }
 
   return processedIrritants;
 }
@@ -194,118 +140,66 @@ async function getIrritants(db) {
   const irritantMap = await selectIrritantMap(db);
   const foodNameMap = await selectFoodNameMap(db);
 
-  const data = [];
-  for (const foodId of irritantMap.keys()) {
-    let edamamIds = edamamIdMap.get(foodId);
-    if (edamamIds === undefined) {
-      edamamIds = [];
-    }
-
-    let irritants = irritantMap.get(foodId);
-    if (irritants === undefined) {
-      irritants = [];
-    }
-    irritants = processIrritants(irritants);
-
-    let names = foodNameMap.get(foodId);
-    if (names === undefined) {
-      names = [];
-    }
-
-    const datum = { foodIds: edamamIds, irritants, names };
-    data.push(datum);
+  function createIrritantEntry(id) {
+    const names = foodNameMap.get(id) ?? [];
+    const foodIds = edamamIdMap.get(id) ?? [];
+    const irritants = irritantMap.get(id) ?? [];
+    return { foodIds, names, irritants: processIrritants(irritants) };
   }
 
-  return data;
+  // combine the edamam/irritant/name multi-maps into a list of { foodIds, names, irritants } objects
+  return Array.from(irritantMap.keys()).map(createIrritantEntry);
 }
 
 async function getFoodGroups(db) {
-  return new Promise((resolve, reject) => {
-    const sql = 'SELECT * FROM food_groups';
-    db.all(sql, [], (err, rows) => {
-      if (err) {
-        reject(err);
-      }
-      const group_names = new Set(rows.map((row) => row.food_group_name));
-      const data = [];
-      for (const row of rows) {
-        const foodGroupName = row.food_group_name;
-        const edamamId = row.edamam_id;
-        const foodName = row.food_name;
-        let foodGroup = data.find((e) => e['name'] === foodGroupName);
-        if (foodGroup === undefined) {
-          foodGroup = { name: foodGroupName, foodRefs: [] };
-          data.push(foodGroup);
-        }
-        const entry = { $: 'EdamamFoodReference', id: edamamId, name: foodName };
-        foodGroup['foodRefs'].push(entry);
-      }
-      resolve(data);
-    });
-  });
+  const sql = 'SELECT * FROM food_groups';
+
+  const keyfunc = (row) => row.food_group_name;
+  const valfunc = (row) => ({ $: 'EdamamFoodReference', id: row.edamam_id, name: row.food_name });
+
+  const groups = multimap(await db.all(sql), keyfunc, valfunc);
+
+  // flatten the food reference multi-map into a list of { name, foodRefs } objects
+  return Array.from(groups.entries()).map(entry => ({ name: entry[0], foodRefs: entry[1] }));
 }
 
-async function deleteCollection(collectionRef, batchSize) {
-  const query = collectionRef.limit(batchSize);
-
-  return new Promise((resolve, reject) => {
-    deleteQueryBatch(firestore, query, resolve).catch(reject);
-  });
-}
-
-async function deleteQueryBatch(firestore, query, resolve) {
-  const snapshot = await query.get();
-
-  const batchSize = snapshot.size;
-  if (batchSize === 0) {
-    // When there are no documents left, we are done
-    resolve();
-    return;
+// Replace all of the documents in the Firestore [collection] with the entries in the [data] array using the provided BulkWriter [writer].
+async function replaceFirestoreCollection(writer, collection, data) {
+  for (const doc of await collection.listDocuments()) {
+    writer.delete(doc);
   }
 
-  // Delete documents in a batch
-  const batch = firestore.batch();
-  snapshot.docs.forEach((doc) => {
-    batch.delete(doc.ref);
-  });
-  await batch.commit();
-
-  // Recurse on the next process tick, to avoid
-  // exploding the stack.
-  process.nextTick(() => {
-    deleteQueryBatch(firestore, query, resolve);
-  });
-}
-
-async function addData(collectionRef, data) {
-  const dataSubset = data.slice(0, maxBatchSize);
-  if (dataSubset.length === 0) {
-    return;
+  for (const entry of data) {
+    writer.create(collection.doc(), entry)
   }
-
-  const batch = firestore.batch();
-  dataSubset.forEach((entry) => {
-    const newRef = collectionRef.doc();
-    batch.set(newRef, entry);
-  });
-  await batch.commit();
-
-  process.nextTick(() => {
-    addData(collectionRef, data.slice(maxBatchSize));
-  });
 }
 
 (async () => {
-  const db = new sqlite3.Database(foodIrritantDbPath, sqlite3.OPEN_READONLY);
-  const foodIrritantData = await getIrritants(db);
-  const foodGroupsData = await getFoodGroups(db);
-  db.close();
+  const dbPath = path.resolve(__dirname, '../data', 'irritants.sqlite3');
 
-  // Delete old data
-  await deleteCollection(foodIrritantsCollection, maxBatchSize);
-  await deleteCollection(foodGroupsCollection, maxBatchSize);
+  console.log(`Opening irritant database at ${dbPath}`);
 
-  // Add new data
-  await addData(foodIrritantsCollection, foodIrritantData);
-  await addData(foodGroupsCollection, foodGroupsData);
+  const db = await sqlite.open({
+    filename: dbPath,
+    driver: sqlite3.cached.Database,
+    mode: sqlite3.OPEN_READONLY
+  });
+
+  const certPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+  const certData = JSON.parse(fs.readFileSync(certPath));
+  const certProject = certData['project_id'];
+
+  console.log(`Initializing Firebase project ${certProject} using credentials from ${certPath}`);
+
+  admin.initializeApp();
+
+  const firestore = admin.firestore();
+  const writer = firestore.bulkWriter();
+
+  await replaceFirestoreCollection(writer, firestore.collection('food_irritants'), await getIrritants(db));
+  await replaceFirestoreCollection(writer, firestore.collection('food_groups'), await getFoodGroups(db));
+  await writer.close();
+  await db.close();
+
+  console.log('Data update complete')
 })();
