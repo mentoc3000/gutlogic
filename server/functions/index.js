b373a203208c48bf39f004367165fdb5ef69b63b
JP Sheehan <jp@gutlogic.co>
2022-08-10 17:43:51 -0400

    Remove pantry cloud functions

--- server/functions/index.js
+++ server/functions/index.js
@@ -4,20 +4,8 @@ admin.initializeApp();
 const functions = require('firebase-functions');
 const food = require('./src/food');
 const auth = require('./src/auth');
-const pantry = require('./src/pantry');
 
 exports.onUserDeleted = functions.auth.user().onDelete(auth.onUserDeleted);
 
 exports.edamamFoodSearch = functions.https.onCall(food.edamamFoodSearch);
 
-exports.onPantryEntryCreated = functions.firestore
-  .document('/user_data/{userId}/pantry/{pantryId}')
-  .onCreate(pantry.onPantryEntryCreated);
-
-exports.onPantryEntryUpdated = functions.firestore
-  .document('/user_data/{userId}/pantry/{pantryId}')
-  .onUpdate(pantry.onPantryEntryUpdated);
-
-exports.onPantryEntryDeleted = functions.firestore
-  .document('/user_data/{userId}/pantry/{pantryId}')
-  .onCreate(pantry.onPantryEntryDeleted);
