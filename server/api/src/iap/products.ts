e7455b7c2c4abd4b4f585d64442e113d56985f55
JP Sheehan <jp@gutlogic.co>
2022-10-15 17:53:22 +0000

    Add IAP products to development environment

--- server/api/src/iap/products.ts
+++ server/api/src/iap/products.ts
@@ -3,9 +3,13 @@ export interface ProductData {
   type: "SUBSCRIPTION";
 }
 
-export const productDataMap: { [productId: string]: ProductData } = {
+export const productDataMap: { [productId: string]: ProductData; } = {
   "premium_subscription_monthly": {
     productId: "premium_subscription_monthly",
+    type: "SUBSCRIPTION",
+  },
+  "premium_subscription_monthly_dev": {
+    productId: "premium_subscription_monthly_dev",
     type: "SUBSCRIPTION",
   },
 };
